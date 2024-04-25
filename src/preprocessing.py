727c596da20108d7f76357566311be14abda26dc
Pikurrot <pikurrot@gmail.com>
2024-04-25 10:13:54 +0200

    Correct save and load tokens

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -205,30 +205,36 @@ def save_tokens(
 	Save tokens to a JSON file.
 	"""
 	tokens = [
-		{
+		[
+			{
 			"tokens": sent["tokens"].tolist(),
 			"spans": sent["spans"].tolist(),
 			"lang": sent["lang"]
-		}
-		for d in tokens for sent in d
+			}
+			for sent in doc
+		]
+		for doc in tokens
 	]
 	with open(path, 'w') as f:
 		json.dump(tokens, f)
 
 def load_tokens(
 		path
-) -> List[Dict[str, Union[np.ndarray, str]]]:
+) -> List[List[Dict[str, np.ndarray, str]]]:
 	"""
 	Load tokens from a JSON file.
 	"""
 	with open(path, 'r') as f:
 		tokens = json.load(f)
 	tokens = [
-		{
+		[
+			{
 			"tokens": np.array(sent["tokens"]),
 			"spans": np.array(sent["spans"]),
 			"lang": sent["lang"]
-		}
-		for sent in tokens
+			}
+			for sent in doc
+		]
+		for doc in tokens
 	]
 	return tokens
