132ce4b2081b906f221db2184711e253b36c0233
Pikurrot <pikurrot@gmail.com>
2024-04-26 19:58:12 +0200

    Correct errors in run_cutext() and add argument cutext_path

--- src/cutext.py
+++ src/cutext.py
@@ -3,7 +3,7 @@ import os
 import json
 from typing import Union
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(os.path.abspath(""))
 
 def run_command(
 		command: str,
@@ -26,9 +26,11 @@ def get_cutext_path() -> Union[str, None]:
 def run_cutext(
 		input_path: str,
 		output_dir: str,
-		verbosity: int=0
+		verbosity: int=0,
+		cutext_path: str=None
 ) -> None:
-	cutext_path = get_cutext_path()
+	if not cutext_path:
+		cutext_path = get_cutext_path()
 	if not cutext_path:
 		raise Exception("cutext path is not defined in config.json")
 	elif not os.path.exists(cutext_path):
@@ -38,10 +40,19 @@ def run_cutext(
 		raise Exception("Input file does not exist")
 	if not os.path.exists(output_dir):
 		os.makedirs(output_dir)
+	# check OS
+	if os.name == 'nt': # Windows
+		if not output_dir.endswith("\\"):
+			output_dir += "\\"
+	elif os.name == 'posix': # Linux or Mac
+		if not output_dir.endswith("/"):
+			output_dir += "/"
 	command = f"java -jar cutext.jar -TM -generateTextFile true -inputFile {input_path} -routeTextFileHashTerms {output_dir}"
 	run_command("cd "+main_dir+" && "+command, verbosity)
 
-def process_input() -> None:
+def process_input(
+		cutext_path: str=None
+) -> None:
 	input_path = os.path.join(ROOT_DIR, "temp", "cutext_in.txt")
 	output_dir = os.path.join(ROOT_DIR, "temp")
-	run_cutext(input_path, output_dir)
+	run_cutext(input_path, output_dir, cutext_path=cutext_path)
