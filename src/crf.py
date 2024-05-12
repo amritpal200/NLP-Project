11ab7dc03cdc0a532dbcc893baf038ea6aab5fcb
Pikurrot <pikurrot@gmail.com>
2024-05-12 14:30:40 +0200

    Complete CRF features definition

--- src/crf.py
+++ src/crf.py
@@ -2,6 +2,7 @@ import pycrfsuite as crfs
 from preprocessing import *
 import os
 import nltk
+import string
 
 ROOT_DIR = os.path.dirname(os.path.abspath(""))
 def put_bio(data, data_tokens, train_data_bio):
@@ -30,6 +31,32 @@ def create_bio_tags(data, data_tokens):
 	put_bio(data, data_tokens, data_bio)
 	return data_bio
 
+def precompute_pos(
+		tokens_path: str,
+		pos_path: str
+):
+	"""
+	Precomputes POS tags for the given tokens and saves them to a file.
+	"""
+	nlp_es, nlp_ca = load_nlps()
+	pos_dir = os.path.dirname(pos_path)
+	if not os.path.exists(pos_dir):
+		os.makedirs(pos_dir)
+	tokens = load_tokens(tokens_path)
+	pos = []
+	for doc in tqdm(tokens):
+		doc_pos = []
+		for sentence in doc:
+			lang = sentence["lang"]
+			if lang == "ca":
+				doc = nlp_ca(" ".join(sentence["tokens"]))
+			else:
+				doc = nlp_es(" ".join(sentence["tokens"]))
+			doc_pos.append([token.pos_ for token in doc])
+		pos.append(doc_pos)
+	with open(pos_path, "w") as f:
+		json.dump(pos, f)
+
 class CRF:
 
 	def __init__(
@@ -42,6 +69,14 @@ class CRF:
 		self.nlp_es, self.nlp_ca = load_nlps()
 		self.nlp_es.disable_pipes('ner', 'parser')
 		self.nlp_ca.disable_pipes('ner', 'parser')
+
+		other_params = ["padding", "before_lim", "after_lim", "special_words"]
+		self.padding = trainer_params.get("padding", False)
+		self.before_lim = trainer_params.get("before_lim", 6)
+		self.after_lim = trainer_params.get("after_lim", 1)
+		self.special_words = trainer_params.get("special_words",\
+			["nada", "ni", "nunca", "ningun", "ninguno", "ninguna", "alguna", "apenas", "para_nada", "ni_siquiera"])
+		trainer_params = {k:v for k,v in trainer_params.items() if k not in other_params}
 
 		if os.path.exists(model_path):
 			self.tagger = crfs.Tagger()
@@ -60,21 +95,82 @@ class CRF:
 
 	def word2features(
 			self,
-			sent: List[Tuple[str, str]],
-			i: int
+			sent: List[Tuple[str, str, str]],
+			idx: int
 	):
 		"""
 		Returns a list of features for a given word in a sentence.
 		"""
-		word, pos, _ = sent[i]
+		word, pos, _ = sent[idx]
 		features = [
 			"bias",
 			"word=" + word,
 			"pos=" + pos,
+			"is_first_capital=" + str(word[0].isupper()),
+			"is_alphanumeric=" + str(word.isalnum()),
+			"has_num=" + str(any(char.isdigit() for char in word)),
+			"has_cap=" + str(any(char.isupper() for char in word)),
+			"has_dash=" + str('-' in word),
+			"has_us=" + str('_' in word),
+			"has_punctuation=" + str(any(char in string.punctuation for char in word)),
+			"is_special=" + str(word.lower() in self.special_words)
 		]
 
-		# add more features
+		# Adding suffix features for suffix lengths of 2, 3, and 4
+		suffix_lengths = [2, 3, 4]
+		for length in suffix_lengths:
+			if len(word) >= length:
+				features.append(f"suffix-{length}=" + word[-length:])
 
+		# Adding prefix features for prefix lengths of 2, 3, and 4
+		prefix_lengths = [2, 3, 4]
+		for length in prefix_lengths:
+			if len(word) >= length:
+				features.append(f"prefix-{length}=" + word[:length])
+
+		pad_token = "<PAD>"
+		if self.padding:
+			# Ensure there are always six bigram features, use padding if necessary
+			for j in range(1, 7):  # Always attempt to generate six bigrams
+				if idx >= j:
+					prev_word = sent[idx - j][0]
+				else:
+					prev_word = pad_token  # Use padding token if there are not enough previous words
+			current_bigram = f"2GRAMBEFORE_{j}={prev_word}_{word}"
+			features.append(current_bigram)
+
+		else:
+			# Adding bigram features for up to 6 words before the current word
+			for j in range(max(0, idx-6), idx):
+				if idx > 0 and j < idx:  # Check to ensure there is a previous word to form a bigram
+					prev_word = sent[j][0]
+				current_bigram = f"2GRAMBEFORE={prev_word}_{word}"
+			features.append(current_bigram)
+
+		# Add a bigram feature for the word after the current word
+		if idx + 1 < len(sent):
+			next_word = sent[idx + 1][0]
+			current_bigram_after = f"2GRAMAFTER={word}_{next_word}"
+			features.append(current_bigram_after)
+		elif self.padding:
+			next_word = pad_token  # Use padding token if there is no next word
+			current_bigram_after = f"2GRAMAFTER={word}_{next_word}"
+			features.append(current_bigram_after)
+
+		# beforepos
+		for i in range(1, self.before_lim + 1):
+			if idx - i >= 0:
+				features.append(f'BEFOREPOS-{i}=' + sent[idx - i][0])
+			elif self.padding:
+				features.append(f'BEFOREPOS-{i}=START')
+		
+		# afterpos
+		for i in range(1, self.after_lim + 1):
+			if idx + i < len(sent):
+				features.append(f'AFTERPOS-{i}=' + sent[idx + i][0])
+			elif self.padding:
+				features.append(f'AFTERPOS-{i}=END')
+		
 		return features
 
 	def sent2features(
@@ -107,7 +203,8 @@ class CRF:
 	def train(
 			self,
 			train_tokens_path: str,
-			train_labels_path: str
+			train_labels_path: str,
+			train_pos_path: Optional[str] = None
 	):
 		"""
 		Trains the CRF model on the given training data.
@@ -118,17 +215,23 @@ class CRF:
 		train_data = load_tokens(train_tokens_path)
 		with open(train_labels_path, "r") as f:
 			train_labels = json.load(f)
+		if train_pos_path is not None:
+			with open(train_pos_path, "r") as f:
+				train_pos = json.load(f)
 
 		sents = []
-		for doc_tokens, doc_labels in tqdm(zip(train_data, train_labels), total=len(train_data)):
+		for d, (doc_tokens, doc_labels) in tqdm(enumerate(zip(train_data, train_labels)), total=len(train_data)):
 			sent = []
-			for sentence, labels in zip(doc_tokens, doc_labels):
-				lang = sentence["lang"]
-				if lang == "ca":
-					doc = self.nlp_ca(" ".join(sentence["tokens"]))
+			for s, (sentence, labels) in enumerate(zip(doc_tokens, doc_labels)):
+				if train_pos_path is not None:
+					pos = train_pos[d][s]
 				else:
-					doc = self.nlp_es(" ".join(sentence["tokens"]))
-				pos = [token.pos_ for token in doc]
+					lang = sentence["lang"]
+					if lang == "ca":
+						doc = self.nlp_ca(" ".join(sentence["tokens"]))
+					else:
+						doc = self.nlp_es(" ".join(sentence["tokens"]))
+					pos = [token.pos_ for token in doc]
 				sent.extend(list(zip(sentence["tokens"], pos, labels)))
 			sents.append(sent)
 
@@ -139,7 +242,9 @@ class CRF:
 			self.trainer.append(xseq, yseq)
 
 		# train and save model
+		print("Training model...")
 		self.trainer.train(self.model_path)
+		print("Model trained")
 
 		self.tagger = crfs.Tagger()
 		self.tagger.open(self.model_path)
